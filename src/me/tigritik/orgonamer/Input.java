362a415242aa3c88a19060dbb0b7bf083b394cf7
eshaan-saxophone <105667485+eshaan-saxophone@users.noreply.github.com>
2022-05-26 14:06:57 -0400

    Update Input.java

--- src/me/tigritik/orgonamer/Input.java
+++ src/me/tigritik/orgonamer/Input.java
@@ -58,11 +58,11 @@ public class Input {
     
   }
 
-  private static final BufferedReader inputReader(String fileName) throws IOException {
+  private final BufferedReader inputReader(String fileName) throws IOException {
     return new BufferedReader(new FileReader(fileName));
   }
 
-  private static final StringTokenizer inputTokenizer() {
+  private final StringTokenizer inputTokenizer() {
     return new StringTokenizer(bf.readLine());
   }
 
